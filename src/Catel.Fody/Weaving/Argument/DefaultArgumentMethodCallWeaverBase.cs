65adccf4f6519e927816735e21cf95fe17386652
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2013-11-19 07:09:07 -0500

    Added missing file

---
+++ src/Catel.Fody/Weaving/Argument/DefaultArgumentMethodCallWeaverBase.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultArgumentMethodCallWeaveBase.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Catel.Fody.Weaving.Argument
+{
+    using System.Collections.Generic;
+
+    using Mono.Cecil;
+    using Mono.Cecil.Cil;
+
+    public abstract class DefaultArgumentMethodCallWeaverBase : ArgumentMethodCallWeaverBase
+    {
+        #region Methods
+        protected override sealed void BuildInstructions(ModuleDefinition module, TypeDefinition type, MethodDefinition method, ParameterDefinition parameter, CustomAttribute attribute, List<Instruction> instructions)
+        {
+            instructions.AddRange(ArgumentInstructionSequenceBuilder.BuildDefaultInstructions(parameter));
+        }
+        #endregion
+    }
+}
