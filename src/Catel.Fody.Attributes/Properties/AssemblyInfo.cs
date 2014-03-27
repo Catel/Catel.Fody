e64a056e72a8966de1aaad817d4600aa7bab72ed
Geert van Horrik <geert@catenalogic.com>
2014-03-27 16:21:24 +0100

    (+) CTL-399 Added signing to Catel.Fody.Attributes assemby

--- src/Catel.Fody.Attributes/Properties/AssemblyInfo.cs
+++ src/Catel.Fody.Attributes/Properties/AssemblyInfo.cs
@@ -1,30 +1,15 @@
-﻿using System.Resources;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Catel development team">
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Catel.Fody.Attributes.PCL")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Catel.Fody.Attributes.PCL")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyTitle("Catel.Fody.Attributes")]
+[assembly: AssemblyProduct("Catel.Fody.Attributes")]
+[assembly: AssemblyDescription("Catel.Fody attributes class")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Sign attributes assembly
+[assembly: AssemblyKeyFile(@"..\..\..\Catel.snk")]
--- src/Catel.Fody/Properties/AssemblyInfo.cs
+++ src/Catel.Fody/Properties/AssemblyInfo.cs
@@ -1,15 +1,12 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="Catel development team">
-//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
 
 using System.Reflection;
 
 [assembly: AssemblyTitle("Catel.Fody")]
-[assembly: AssemblyDescription("Catel plugin for Fody")]
-[assembly: AssemblyCompany("CatenaLogic")]
 [assembly: AssemblyProduct("Catel.Fody")]
-[assembly: AssemblyCopyright("Copyright © CatenaLogic 2012 - 2013")]
-[assembly: AssemblyVersion("1.5.4")]
-[assembly: AssemblyInformationalVersion("1.5.4")]
+[assembly: AssemblyDescription("Catel plugin for Fody")]
