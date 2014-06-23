aebf2757636fb8af1507babc873b7302e5c9f0ec
Geert van Horrik <geert@catenalogic.com>
2014-06-23 16:36:38 +0200

    (x) CTL-472 Added unit tests to make sure the LoggingWeaver no longer has this issue

--- src/Catel.Fody.TestAssembly/LoggingClass.cs
+++ src/Catel.Fody.TestAssembly/LoggingClass.cs
@@ -9,11 +9,20 @@ namespace Catel.Fody.TestAssembly
 {
     using Logging;
 
+    public static class MyCustomLogManager
+    {
+        public static ILog GetCurrentClassLogger()
+        {
+            return LogManager.GetCurrentClassLogger();
+        }
+    }
+
     public class LoggingClass
     {
         #region Constants
         private static readonly ILog AutoLog = LogManager.GetCurrentClassLogger();
         private static readonly ILog ManualLog = LogManager.GetLogger(typeof (LoggingClass));
+        private static readonly ILog CustomLog = MyCustomLogManager.GetCurrentClassLogger();
         #endregion
 
         public LoggingClass()
