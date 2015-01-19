a1bf463b2c2925a7d133801dc6cff9f7090f7995
Geert van Horrik <geert@catenalogic.com>
2015-01-19 13:59:49 +0100

    Possible build fix

--- src/Catel.Fody/Weaving/Argument/Implementations/IsNotNullArgumentMethodCallWeaver.cs
+++ src/Catel.Fody/Weaving/Argument/Implementations/IsNotNullArgumentMethodCallWeaver.cs
@@ -14,7 +14,11 @@ namespace Catel.Fody.Weaving.Argument
     {
         protected override void SelectMethod(TypeDefinition argumentTypeDefinition, TypeReference typeToCheck, out MethodDefinition selectedMethod)
         {
-            selectedMethod = argumentTypeDefinition.Methods.FirstOrDefault(definition => definition.Name == "IsNotNull" && definition.Parameters.Count == 2);
+            selectedMethod = argumentTypeDefinition.Methods.FirstOrDefault(definition => definition.Name == "IsNotNull" && definition.Parameters.Count == 2 && string.Equals(definition.Parameters[1].ParameterType.FullName, typeToCheck.FullName));
+            if (selectedMethod == null)
+            {
+                selectedMethod = argumentTypeDefinition.Methods.FirstOrDefault(definition => definition.Name == "IsNotNull" && definition.Parameters.Count == 2);
+            }
         }
     }
 }
