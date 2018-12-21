aa35c223d01f76a767b4492680c0374673be5326
Geert van Horrik <geert@catenalogic.com>
2018-12-21 15:07:29 +0100

    Add expected code to easily compare the changes

--- src/Catel.Fody.TestAssembly/ObservableObjectTest.cs
+++ src/Catel.Fody.TestAssembly/ObservableObjectTest.cs
@@ -54,4 +54,99 @@
             OnLastNameWithWrongCallbackChangedCallbackCalled = true;
         }
     }
+
+    [NoWeaving]
+    public class ObservableObjectTest_Expected : ObservableObject
+    {
+        private bool _onFirstNameChangedCallbackCalled;
+        private bool _onLastNameChangedCallbackCalled;
+        private bool _onLastNameWithWrongCallbackChangedCallbackCalled;
+        private string _existingProperty;
+        private string _firstName;
+        private string _lastName;
+        private string _lastNameWithWrongCallback;
+
+        public bool OnFirstNameChangedCallbackCalled
+        {
+            get => _onFirstNameChangedCallbackCalled;
+            set => _onFirstNameChangedCallbackCalled = value;
+        }
+
+        public bool OnLastNameChangedCallbackCalled
+        {
+            get => _onLastNameChangedCallbackCalled;
+            set => _onLastNameChangedCallbackCalled = value;
+        }
+
+        public bool OnLastNameWithWrongCallbackChangedCallbackCalled
+        {
+            get => _onLastNameWithWrongCallbackChangedCallbackCalled;
+            set => _onLastNameWithWrongCallbackChangedCallbackCalled = value;
+        }
+
+        public string ExistingProperty
+        {
+            get => _existingProperty;
+            set
+            {
+                _existingProperty = value;
+                RaisePropertyChanged(nameof(ExistingProperty));
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnFirstNameChanged();
+                RaisePropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnLastNameChanged();
+                RaisePropertyChanged(nameof(LastName));
+            }
+        }
+
+        public string LastNameWithWrongCallback
+        {
+            get => _lastNameWithWrongCallback;
+            set
+            {
+                _lastNameWithWrongCallback = value;
+                RaisePropertyChanged(nameof(LastNameWithWrongCallback));
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        private void OnFirstNameChanged()
+        {
+            OnFirstNameChangedCallbackCalled = true;
+        }
+
+        private void OnLastNameChanged()
+        {
+            OnLastNameChangedCallbackCalled = true;
+        }
+
+        private void OnLastNameWithWrongCallbackChanged(object sender)
+        {
+            OnLastNameWithWrongCallbackChangedCallbackCalled = true;
+        }
+    }
 }
