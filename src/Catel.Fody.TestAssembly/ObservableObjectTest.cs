a16fd4187c930436fd408846b02f4bfecea209dd
Geert van Horrik <geert@catenalogic.com>
2018-12-21 15:03:16 +0100

    Write unit tests for ObservableObject weaving

---
+++ src/Catel.Fody.TestAssembly/ObservableObjectTest.cs
@@ -0,0 +1,57 @@
+﻿namespace Catel.Fody.TestAssembly
+{
+    using Catel.Data;
+
+    public class ObservableObjectTest : ObservableObject
+    {
+        private string _existingProperty;
+
+        [NoWeaving]
+        public bool OnFirstNameChangedCallbackCalled { get; set; }
+
+        [NoWeaving]
+        public bool OnLastNameChangedCallbackCalled { get; set; }
+
+        [NoWeaving]
+        public bool OnLastNameWithWrongCallbackChangedCallbackCalled { get; set; }
+
+        public string ExistingProperty
+        {
+            get => _existingProperty;
+            set
+            {
+                _existingProperty = value;
+                RaisePropertyChanged(nameof(ExistingProperty));
+            }
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string LastNameWithWrongCallback { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+
+        private void OnFirstNameChanged()
+        {
+            OnFirstNameChangedCallbackCalled = true;
+        }
+
+        private void OnLastNameChanged()
+        {
+            OnLastNameChangedCallbackCalled = true;
+        }
+
+        private void OnLastNameWithWrongCallbackChanged(object sender)
+        {
+            OnLastNameWithWrongCallbackChangedCallbackCalled = true;
+        }
+    }
+}
---
+++ src/Catel.Fody.Tests/ObservableObjectFacts.cs
@@ -0,0 +1,48 @@
+﻿namespace Catel.Fody.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ObservableObjectFacts
+    {
+        [TestCase]
+        public void DoesNotWeaveExistingProperties()
+        {
+            var type = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.ObservableObjectTest");
+            var oo = (dynamic)Activator.CreateInstance(type);
+
+            oo.ExistingProperty = "hi there";
+
+            Assert.AreEqual("hi there", oo.ExistingProperty);
+        }
+
+        [TestCase]
+        public void IgnoresPropertiesWithoutBackingField()
+        {
+            // TODO: test Title property
+        }
+
+        [TestCase]
+        public void HandlesChangeNotificationsMethodsCorrectly()
+        {
+            var type = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.ObservableObjectTest");
+            var oo = (dynamic)Activator.CreateInstance(type);
+
+            Assert.IsFalse(oo.OnFirstNameChangedCallbackCalled);
+            oo.FirstName = "change";
+            Assert.IsTrue(oo.OnFirstNameChangedCallbackCalled);
+        }
+
+        [TestCase]
+        public void IgnoresChangeNotificationsWithoutRightSignature()
+        {
+            var type = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.ObservableObjectTest");
+            var oo = (dynamic)Activator.CreateInstance(type);
+
+            Assert.IsFalse(oo.LastNameWithWrongCallback);
+            oo.LastNameWithWrongCallback = "change";
+            Assert.IsFalse(oo.LastNameWithWrongCallback);
+        }
+    }
+}
