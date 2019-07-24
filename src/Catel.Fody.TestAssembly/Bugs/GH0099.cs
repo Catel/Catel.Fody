2179d2c802f9a7097562c31ef4c4f8d70d087a92
Geert van Horrik <geert@catenalogic.com>
2019-07-24 10:39:21 +0200

    #99 Add repro for BadImageException for generic types

---
+++ src/Catel.Fody.TestAssembly/Bugs/GH0099.cs
@@ -0,0 +1,53 @@
+﻿namespace Catel.Fody.TestAssembly.Bugs.GH0099
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Catel.MVVM;
+
+    public class TestViewModel : ViewModelBase
+    {
+        [Model]
+        [Expose("ID")]
+        public Model Model { get; set; }
+    }
+
+    public class Model : BaseModel<Model>
+    {
+
+    }
+
+    public class BaseModel<T> : EntityBaseWithID<int, T>
+        where T : BaseModel<T>
+    {
+
+    }
+
+    public class EntityBaseWithID<T, S>
+        where S : EntityBaseWithID<T, S>
+    {
+        public T ID { get; set; }
+
+        //this is why I'm using S
+        public override bool Equals(object other)
+        {
+            if (other is S n)
+            {
+                return n.ID.Equals(ID);
+            }
+
+            return false;
+        }
+
+        //irrelevant for this example
+        public override int GetHashCode()
+        {
+            var hashCode = 509650306;
+            hashCode = hashCode * -1521134295 + EqualityComparer<T>.Default.GetHashCode(ID);
+            hashCode = hashCode * -1521134295 + GetType().GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ src/Catel.Fody.Tests/Repros/GH0099.cs
@@ -0,0 +1,18 @@
+﻿namespace Catel.Fody.Tests.Repros
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GH0099TestFixture
+    {
+        [TestCase]
+        public void WeavingArgumentCheckForUnusedArgument()
+        {
+            var type = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.Bugs.GH0099.TestViewModel");
+            var instance = Activator.CreateInstance(type) as dynamic;
+
+            Assert.IsNotNull(instance);
+        }
+    }
+}
