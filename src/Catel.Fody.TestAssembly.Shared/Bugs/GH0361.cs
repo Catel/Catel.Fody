8515488974b0797b949e581274e3847b3725541e
Przemek Sulikowski <P.Sulikowski@osadkowski.pl>
2021-08-09 12:23:54 +0200

    Add unit test for SetValue override selection issue

---
+++ src/Catel.Fody.TestAssembly.Shared/Bugs/GH0361.cs
@@ -0,0 +1,22 @@
+﻿namespace Catel.Fody.TestAssembly.Bugs.GH0361
+{
+    using Catel.MVVM;
+
+    public class MyDerivedViewModel : ViewModelBase
+    {
+        [Model]
+        public TestModel Model { get; set; }
+        [ViewModelToModel]
+        public object Property { get; set; }
+
+        public void SetValue(string s)
+        {
+
+        }
+    }
+
+    public class TestModel
+    {
+        public object Property { get; set; }
+    }
+}
