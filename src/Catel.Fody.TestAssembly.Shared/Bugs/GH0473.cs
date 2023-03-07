4473d3bf2c0a4c6bc9f7c6cfaa0d7e5889d60fe9
Geert van Horrik <geert@catenalogic.com>
2023-03-07 10:04:43 +0100

    Create failing test for GH0473

---
+++ src/Catel.Fody.TestAssembly.Shared/Bugs/GH0473.cs
@@ -0,0 +1,16 @@
+﻿namespace Catel.Fody.TestAssembly.Bugs.GH0473
+{
+    using Catel.MVVM;
+
+    public class GH0473ViewModel : ViewModelBase
+    {
+        [Model]
+        [Expose(nameof(TestModel.Property))]
+        public TestModel Model { get; }
+    }
+
+    public class TestModel
+    {
+        public object Property { get; set; }
+    }
+}
---
+++ src/Catel.Fody.Tests.Shared/Repros/GH0473.cs
@@ -0,0 +1,18 @@
+﻿namespace Catel.Fody.Tests.Repros
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GH0473TestFixture
+    {
+        [TestCase]
+        public void Weaving_Init_Only_Model_Properties()
+        {
+            var viewModelType = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.Bugs.GH0473.GH0473ViewModel");
+            var viewModel = Activator.CreateInstance(viewModelType) as dynamic;
+
+            viewModel.Property = new object();
+        }
+    }
+}
