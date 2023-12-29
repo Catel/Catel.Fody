9b6f2b4aad471aa5396cf79b391c6a63e424bf8e
Geert van Horrik <geert@catenalogic.com>
2023-12-29 16:44:33 +0100

    Add unit tests for #531

---
+++ src/Catel.Fody.TestAssembly.Shared/Bugs/GH0531.cs
@@ -0,0 +1,36 @@
+﻿namespace Catel.Fody.TestAssembly.Bugs.GH0531
+{
+    using Catel.Data;
+    using Catel.MVVM;
+    using System.Diagnostics;
+
+    public class TestModel : ObservableObject
+    {
+        private int _counter;
+
+        public object MyProperty { get; set; }
+
+        public int Counter { get => _counter; }
+
+        private void OnMyPropertyChanged()
+        {
+            _counter++;
+
+            Debug.WriteLine($"Property changed: {MyProperty} {_counter}");
+        }
+    }
+
+    public class MyDerivedViewModel : ViewModelBase
+    {
+        public MyDerivedViewModel(TestModel testModel)
+        {
+            Model = testModel;
+        }
+
+        [Model]
+        public TestModel Model { get; set; }
+
+        [ViewModelToModel]
+        public object MyProperty { get; set; }
+    }
+}
---
+++ src/Catel.Fody.Tests.Shared/Repros/GH0531.cs
@@ -0,0 +1,28 @@
+﻿namespace Catel.Fody.Tests.Repros
+{
+    using System;
+    using System.ComponentModel;
+    using Catel.MVVM;
+    using Catel.Reflection;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GH0531TestFixture
+    {
+        [TestCase]
+        public void Raises_PropertyChanged_Count_Correctly()
+        {
+            var modelType = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.Bugs.GH0531.TestModel");
+            var model = Activator.CreateInstance(modelType);
+
+            var viewModelType = AssemblyWeaver.Instance.Assembly.GetType("Catel.Fody.TestAssembly.Bugs.GH0531.MyDerivedViewModel");
+            var viewModel = Activator.CreateInstance(viewModelType, model) as dynamic;
+
+            Assert.That(viewModel.Model.Counter, Is.EqualTo(0));
+
+            PropertyHelper.SetPropertyValue(model, "MyProperty", "test");
+
+            Assert.That(viewModel.Model.Counter, Is.EqualTo(1));
+        }
+    }
+}
